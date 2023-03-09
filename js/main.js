ecdb1f40b76d36b03e903670ef58b00b040d7f15
Nathan Correia <naathancaue556@gmail.com>
2023-03-09 18:53:57 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('#buttons-container button')
+
+
+console.log(buttons)
+
+const calc = document.querySelector('.calcular').addEventListener('click', calcular)
+
+function calcular() {
+    
+}
+
