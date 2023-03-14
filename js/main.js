7b584a7e10a9578dd4c2125ee2a88f28204feacf
Nathan Correia <naathancaue556@gmail.com>
2023-03-14 20:33:23 -0300

    Atualização Tip

--- js/main.js
+++ js/main.js
@@ -1,11 +1,55 @@
-const buttons = document.querySelectorAll('#buttons-container button')
+const cincoPorcento = document.querySelector('#five-percent').addEventListener('click', somarcinco)
 
-
-console.log(buttons)
-
-const calc = document.querySelector('.calcular').addEventListener('click', calcular)
-
-function calcular() {
+function somarcinco(){
+    const bill = document.querySelector('#bill-price').value
+    const tipAmount = document.querySelector('#value-tip-amount')
     
+
+    fivePercent = bill * 0.05
+    tipAmount.innerHTML = fivePercent.toFixed(2)
+}
+
+const dezPorcento = document.querySelector('#ten-percent').addEventListener('click', somardez)
+
+function somardez(){
+    const bill = document.querySelector('#bill-price').value
+    const tipAmount = document.querySelector('#value-tip-amount')
+    
+
+    fivePercent = bill * 0.1
+    tipAmount.innerHTML = fivePercent.toFixed(2)
+}
+
+const quinzePorcento = document.querySelector('#five-teen-percent').addEventListener('click', somarquinze)
+
+function somarquinze(){
+    const bill = document.querySelector('#bill-price').value
+    const tipAmount = document.querySelector('#value-tip-amount')
+    
+
+    fivePercent = bill * 0.15
+    tipAmount.innerHTML = fivePercent.toFixed(2)
+}
+
+const vintecincoPorcento = document.querySelector('#twenty-five-percent').addEventListener('click', somarvintecinco)
+
+function somarvintecinco(){
+    const bill = document.querySelector('#bill-price').value
+    const tipAmount = document.querySelector('#value-tip-amount')
+    
+
+    fivePercent = bill * 0.25
+    tipAmount.innerHTML = fivePercent.toFixed(2)
+}
+
+const cincoCinquenta = document.querySelector('#fifty-percent').addEventListener('click', somarcinquenta)
+
+function somarcinquenta(){
+    const bill = document.querySelector('#bill-price').value
+    const tipAmount = document.querySelector('#value-tip-amount')
+    
+
+    fivePercent = bill * 0.50
+    tipAmount.innerHTML = fivePercent.toFixed(2)
 }
 
